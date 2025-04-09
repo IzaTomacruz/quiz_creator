30435e320f7767a66ea1d400a7378ba2a1ac1a4a
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-04-09 20:42:18 +0800

    created a loop and ask to input the question, possible answers,and the correct answer

--- quiz_creator_program.py
+++ quiz_creator_program.py
@@ -1,10 +1,18 @@
 # Create a loop that will allow you to create a question and answer again
-
+while True:
 # Inside the loop:
 # Ask enter a question
-# Ask to enter choices for A, B, C, and D
-# Ask enter the correct answer
+    question = input("Enter a question: ")
 
+# Ask to enter choices for A, B, C, and D
+    choice_a = input("Enter answer for A: ")
+    choice_b = input("Enter answer for B: ")
+    choice_c = input("Enter answer for C: ")
+    choice_d = input("Enter answer for D: ")
+
+# Ask enter the correct answer
+    correct_ans = input("Enter the correct answer: ")
+    
 # Ask if you want to save
 # if yes, all the question and answer will be writen to another file
 # if no, it will not be saved and automatically proceed
@@ -12,4 +20,4 @@
 # Ask if you want to enter a question and answer again
 # If yes, it will ask to input the questions and answers again
 # if no, it will exit the loop and close the program
-
+    break
