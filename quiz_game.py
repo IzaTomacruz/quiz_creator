e11f6d9192045ba422c861b82ae90415f3557ef4
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-05-04 15:46:04 +0800

    create pseudocode for the quiz_game

---
+++ quiz_game.py
@@ -0,0 +1,12 @@
+# Setup the GUI
+    # Colors
+    # Backgrounds
+    # Fonts
+    # Buttons
+# Load random questions (10 items)
+# Check answer
+    # if correct, show correct!
+    # if not correct, show the correct answer
+# show score
+# ask if try again
+# close if done
